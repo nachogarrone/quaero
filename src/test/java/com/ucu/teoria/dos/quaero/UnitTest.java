3cdbbc07001b99b48df9666331215775439256fc
nachogarrone <nachogarrone@gmail.com>
2017-07-04 19:53:56 -0300

    Test added

---
+++ src/test/java/com/ucu/teoria/dos/quaero/UnitTest.java
@@ -0,0 +1,14 @@
+package com.ucu.teoria.dos.quaero;
+
+import org.junit.Test;
+
+/**
+ * Created by nachogarrone on 7/4/17.
+ */
+public class UnitTest {
+
+    @Test
+    public void test1(){
+
+    }
+}
